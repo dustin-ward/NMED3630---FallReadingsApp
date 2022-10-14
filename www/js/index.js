c187a434a6eec2f8834084a938b7a0bdd7076523
dustin-ward <wardddustin@gmail.com>
2022-10-14 14:07:07 -0600

    Added pages

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,27 @@
 var app = new Framework7({
     // App root element
     el: '#app',
-    // ... other parameters
+    routes: [
+        {
+            path: '/',
+            url: 'index.html',
+        },
+        {
+            path: '/readings/',
+            url: 'pages/readings.html',
+        },
+        {
+            path: '/music/',
+            url: 'pages/music.html',
+        },
+        {
+            path: '/shows/',
+            url: 'pages/shows.html',
+        },
+        {
+            path: '/otherMedia/',
+            url: 'pages/other_media.html',
+        }
+    ]
    });
 var mainView = app.views.create('.view-main');
